052424c4a0aff68e6a3a5ece75a9c4c07b4f9b13
Miguel Ortiz | WebDev <elsuperm2016@gmail.com>
2023-12-22 11:49:32 -0500

    Create Repository

---
+++ ActividadSena/src/actividadsena/ActividadSena.java
@@ -0,0 +1,12 @@
+package actividadsena;
+
+public class ActividadSena {
+
+    public static void main(String[] args) {
+
+        int numero = 4654;
+        System.out.println(numero);        
+        
+    }
+    
+}
---
+++ actividadSenaJava/src/actividadsenajava/ActividadSenaJava.java
@@ -0,0 +1,51 @@
+package actividadsenajava;
+
+/**
+ *
+ * @author elsup
+ */
+public class ActividadSenaJava {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+
+        double a = 2.374;
+        double b = 13.25;
+        double c = 1.43;
+        double d = 1.1;
+
+        double resultado;
+
+        resultado = (4 * a) + (5 * b) + c - d * (3 * a * b * c) - a + b + c;
+        System.out.println(resultado);
+
+        resultado = 9 / 3 * 12 + 16 - 8;
+        System.out.println(resultado);
+
+        resultado = 2 * 4 + 2.5 - 1.5 * 3 + 2 * 1 / 2.5;
+        System.out.println(resultado);
+
+        resultado = 5 * 7 + 3 / 2 + 4;
+        System.out.println(resultado);
+
+        double aa = 2;
+        double bb = 1.5;
+        double cc = 3.7;
+
+        resultado = 2 * aa + bb - aa + bb + cc + 5 * cc;
+        System.out.println(resultado);
+
+        resultado = 35 - 2 * 6 / 2 - 4 + 2 + 1.5 - 6 * 5 * 2;
+        System.out.println(resultado);
+
+        resultado = 7*5-2+32+8/1.6+1.5*0.8;
+        System.out.println(resultado);
+
+        resultado = 5*7+3/2+4;
+        System.out.println(resultado);
+
+    }
+
+}
